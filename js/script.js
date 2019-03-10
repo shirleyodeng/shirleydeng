3a4d8f7368f5918aaf8fe17310d6e1a6640e64a4
Shirley Deng <shirleeey@live.com>
2019-03-10 22:15:18 +0000

    changes to work page

--- js/script.js
+++ js/script.js
@@ -20,6 +20,23 @@ $(document).ready(function() {
   $(".nav-bar-link").mouseleave(function(e) {
     $(e.target)[0].innerText = $(e.target)[0].innerText.replace(" HARD", "")
   });
+
+  // Work
+  $('.project-link').click(function(e) {
+    var name = $(e.target).text()
+    if (name === "European Rugby") {
+      europeanRugby()
+    } else if (name === "Gifter") {
+      gifter()
+    } else if (name === "MyCineplace") {
+      mycineplace()
+    }
+  });
+
+  $('.project-close').click(function(e) {
+    $(".project-container").addClass("d-none")
+    $(".projects").removeClass("d-none")
+  });
 });
 
 function toggleBackToTopButton() {
@@ -29,4 +46,41 @@ function toggleBackToTopButton() {
   } else {
     $('.btt').addClass("d-none")
   }
+}
+
+function europeanRugby() {
+  var name = "European Rugby"
+  var imageName = "european-rugby"
+  var blurb = "A bespoke marketing tool designed to engage brands with the world's biggest club rugby competitions"
+  var techStack = "<div class='col-md-6'><div>Ruby on Rails</div><div>PostgreSQL</div><div>Git</div></div><div class='col-md-6'><div>HTML5</div><div>CSS3</div><div>Bootstrap</div><div>jQuery</div></div>"
+  var spec = "<div>> Easy-to-use admin system to create new pages</div><div>> Relational database schema to represent and support processes</div><div>> Responsive web design</div>"
+  handleProject(name, imageName, blurb, techStack, spec)
+}
+
+function gifter() {
+  var name = "Gifter"
+  var imageName = "gifter"
+  var blurb = "A prototype of a web application that makes it easy for parents to start investing in their child's future by crowdfunding towards purposeful gifts and experiences.<br/><br/>View website"
+  var techStack = "<div class='col-md-6'><div>Ruby on Rails</div><div>PostgreSQL</div><div>Git</div></div><div class='col-md-6'><div>HTML5</div><div>CSS3</div><div>Bootstrap</div><div>jQuery</div></div>"
+  var spec = "<div>> Facebook login to create a smooth user registration process</div><div>> Relational database schema to represent and support processes</div><div>> Responsive web design</div>"
+  handleProject(name, imageName, blurb, techStack, spec)
+}
+
+function mycineplace() {
+  var name = "MyCineplace"
+  var imageName = "mycineplace"
+  var blurb = "A platform for all things cinema.<br/><br/>View website"
+  var techStack = "<div class='col-md-6'><div>Ruby on Rails</div><div>PostgreSQL</div><div>Git</div></div><div class='col-md-6'><div>HTML5</div><div>CSS3</div><div>Bootstrap</div><div>jQuery</div></div>"
+  var spec = "<div>> Admin data management system</div><div>> Data integration through external APIs and web scraping</div><div>> Facebook + Google login to create a smooth user registration process</div><div>> Relational database schema to represent and support processes</div><div>> Responsive web design</div>"
+  handleProject(name, imageName, blurb, techStack, spec)
+}
+
+function handleProject(name, imageName, blurb, techStack, spec) {
+  $(".project-title").text(name)
+  $(".project-image")[0].outerHTML = "<img src='images/" + imageName + ".png' class='project-image'>"
+  $(".project-blurb").html(blurb)
+  $(".project-stack").html(techStack)
+  $(".project-spec").html(spec)
+  $(".projects").addClass("d-none")
+  $(".project-container").removeClass("d-none")
 }
