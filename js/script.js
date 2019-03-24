62d43fad79cb812b7945ec72470200bee9ed7793
Shirley Deng <shirleeey@live.com>
2019-03-24 18:58:05 +0000

    work redesign

--- js/script.js
+++ js/script.js
@@ -61,16 +61,16 @@ function europeanRugby() {
 function gifter() {
   var name = "Gifter"
   var imageName = "gifter"
-  var blurb = "A prototype of a web application that makes it easy for parents to start investing in their child's future by crowdfunding towards purposeful gifts and experiences.<br/><br/><a href='https://www.gifter.tech' target='_blank'>Go to <img src='images/back-arrow.png' class='flip-hor'> website</a>"
+  var blurb = "A prototype of a web application that makes it easy for parents to start investing in their child's future by crowdfunding towards purposeful gifts and experiences.<br/><br/>Go on, have a browse <img src='images/back-arrow.png' class='flip-hor'><a href='https://www.gifter.tech' target='_blank'> go to website</a>"
   var techStack = "<div class='col-md-6'><div>Ruby on Rails</div><div>PostgreSQL</div><div>Heroku</div><div>Git</div></div><div class='col-md-6'><div>HTML5</div><div>CSS3</div><div>Bootstrap</div><div>jQuery</div></div>"
-  var spec = "<div>> Facebook login to create an easy user registration process</div><div>> Messaging system between event guests</div><div>> Relational database schema to represent and support processes</div><div>> Responsive web design</div><div>> Stripe integration to receive and send payments</div>"
+  var spec = "<div>> Facebook login to create an easy user registration process</div><div>> Fun, interactive event dashboard designed to engage children</div><div>> Messaging system for events</div><div>> Relational database schema to represent and support processes</div><div>> Responsive web design</div><div>> Stripe integration to receive and send payments</div>"
   handleProject(name, imageName, blurb, techStack, spec)
 }
 
 function mycineplace() {
   var name = "MyCineplace"
   var imageName = "mycineplace"
-  var blurb = "A one stop platform for all things cinema - discover content, watch trailers, read reviews, find showtimes.<br/><br/><a href='https://www.mycineplace.com' target='_blank'>Go to <img src='images/back-arrow.png' class='flip-hor'> website</a>"
+  var blurb = "A one stop platform for all things cinema - discover content, watch trailers, read reviews, find showtimes.<br/><br/>Go on, have a browse <img src='images/back-arrow.png' class='flip-hor'><a href='https://www.mycineplace.com' target='_blank'> go to website</a>"
   var techStack = "<div class='col-md-6'><div>Ruby on Rails</div><div>PostgreSQL</div><div>Capistrano</div><div>Git</div></div><div class='col-md-6'><div>HTML5</div><div>CSS3</div><div>Bootstrap</div><div>jQuery</div></div>"
   var spec = "<div>> Admin data management system</div><div>> Data integration through external APIs and web scraping</div><div>> Facebook + Google login to create a smooth user registration process</div><div>> Clean, flowing user browsing journey</div><div>> Relational database schema to represent and support processes</div><div>> Responsive web design</div><div>> User location detection to create a personal experience</div>"
   handleProject(name, imageName, blurb, techStack, spec)
